75d43a7f09bd32376915616ee242d2e6d8aa201b
rachel <seeyou0727@gmail.com>
2026-01-28 15:37:44 +0800

    update main.js

--- src/main.js
+++ src/main.js
@@ -21,29 +21,5 @@ if (homePageBtn) {
 
 renderHourlyForecast();
 
-// 假資料 (以後從 API 抓)
-// const fakeData = {
-//   ok: true,
-//   renderData: {
-//     minT: 18,
-//     maxT: 32,
-//     barLeft: '25', // 模擬算出 25%
-//     barWidth: '40', // 模擬算出 40%
-//     weather: '雷陣雨',
-//     comfortIndex: '悶熱',
-//     chanceOfRain: 60,
-//   },
-// };
-
-// 假資料 (以後從 API 抓)
-// const fakeErrorData = {
-//   ok: false,
-//   error: {
-//     message: '無法連線到氣象資料服務，請稍後再試。',
-//   },
-// };
-
 const card2_Data = await getCard2RenderData();
-console.log(card2_Data);
-
 renderDailySummary(card2_Data);
